50e5b1d62850c5dd63d8530fcda085d685f9bf0a
Urunov <abuurunov31@gmail.com>
2026-09-13 19:50:39 +0300

    Fix PEP8 errors in test_program.py

--- test_program.py
+++ test_program.py
@@ -1,21 +1,23 @@
-try:
-    import practicum
-except ImportError:
-    raise AssertionError('Модуль `practicum` не обнаружен.')
+"""Тесты для проверки модуля practicum."""
 
-EXPECTED_FUNC_NAME = 'say_hello'
+import practicum
+
+EXPECTED_FUNC_NAME = "say_hello"
 
 
 def test_say_hello_exists():
+    """Проверить, что функция say_hello есть в модуле practicum."""
     assert hasattr(practicum, EXPECTED_FUNC_NAME), (
-        f'Функция `{EXPECTED_FUNC_NAME}` не обнаружена в модуле `practicum`')
+        f"Функция {EXPECTED_FUNC_NAME} не обнаружена в модуле practicum"
+    )
 
 
 def test_say_hello_run_without_exceptions():
+    """Проверить, что say_hello запускается без исключений."""
     try:
         practicum.say_hello()
     except Exception as error:
         raise AssertionError(
-            f'При запуске функции `{EXPECTED_FUNC_NAME} ` возникло '
-            f'исключение: {type(error).__name__}: {error} `'
+            f"При запуске функции {EXPECTED_FUNC_NAME} возникло "
+            f"исключение: {type(error).name}: {error}"
         )
